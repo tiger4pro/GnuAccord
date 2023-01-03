7e596adac212337ebc1003728bc0c036306e9e91
tiger4pro <sprudhom@gmail.com>
2023-01-03 14:27:52 -0500

    00.00.03 Il semble que vous avez rencontré une erreur lors de l'exécution de ce code. L'erreur "Replacement index 18 out of range for positional args tuple" indique qu'il y a un problème avec l'une des valeurs que vous utilisez pour formater la chaîne de caractères.
    Le problème se produit dans cette ligne de code:
    
    f.write("  {}{}1{} {}{}3{} | {}{}5{} {}{}6{} | {}{}8{} {}{}10{} | {}{}12{} {}{}13{}\n".format(note, octave, dur, note, octave+1, dur, note, octave+2, dur, note, octave+3, dur, note, octave+4, dur, note, octave+5, dur))
    
    Le formatage de chaîne de caractères utilise des accolades {} pour indiquer les emplacements où les valeurs doivent être insérées dans la chaîne de caractères. Chaque accolade est remplacée par une valeur fournie dans l'appel à la méthode .format(). Dans ce cas, il y a 18 accolades, ce qui signifie qu'il devrait y avoir 18 valeurs fournies.
    
    Il semble que le nombre de valeurs fournies est insuffisant par rapport au nombre d'accolades. Pour résoudre ce problème, vous devez vous assurer qu'il y a autant de valeurs que d'accolades dans la chaîne de caractères.
    
    Voici comment le code devrait être modifié pour éviter cette erreur:
    
    f.write("  {}{}1{} {}{}3{} | {}{}5{} {}{}6{} | {}{}8{} {}{}10{} | {}{}12{} {}{}13{}\n".format(note, octave, dur, note, octave, dur, note, octave, dur, note, octave, dur, note, octave, dur, note, octave, dur))
    
    Je vous recommande également de vérifier que les autres valeurs utilisées dans ce code sont correctes et ont les types de données attendus. Si vous rencontrez d'autres erreurs, n'hésitez pas à me poser des questions.

---
+++ 1_do_majeur_quadruplecroche.ly
@@ -0,0 +1,9 @@
+\version "2.20.0"
+
+% do majeur
+{
+  \time 4/4
+  \tempo 4=100
+  \key do \major
+  \autoBeamOff
+  s1
--- GNUAccord/GNUAccord.py
+++ GNUAccord/GNUAccord.py
@@ -1,19 +1,19 @@
-# Noms des notes de la musique en français
-notes = ['do', 'do#', 'ré', 'ré#', 'mi', 'fa', 'fa#', 'sol', 'sol#', 'la', 'la#', 'si']
+ï»¿# Noms des notes de la musique en franÃ§ais
+notes = ['do', 'do#', 'rÃ©', 'rÃ©#', 'mi', 'fa', 'fa#', 'sol', 'sol#', 'la', 'la#', 'si']
 
-# Durée de la quadruple croche
+# DurÃ©e de la quadruple croche
 dur = "\\quadruplecroche"
 
-# Boucle sur tous les octaves (de 1 à 10)
+# Boucle sur tous les octaves (de 1 Ã  10)
 for octave in range(1, 11):
   # Boucle sur toutes les notes de la gamme
   for note in notes:
-    # Crée le nom du fichier
+    # CrÃ©e le nom du fichier
     filename = "{}_{}_majeur_{}.ly".format(octave, note, dur)
     
-    # Ouvre le fichier en mode écriture
+    # Ouvre le fichier en mode Ã©criture
     with open(filename, 'w') as f:
-      # Écrit le contenu du fichier
+      # Ã‰crit le contenu du fichier
       f.write("\\version \"2.20.0\"\n")
       f.write("\n")
       f.write("% {} majeur\n".format(note))
@@ -23,7 +23,7 @@ for octave in range(1, 11):
       f.write("  \\key {} \\major\n".format(note))
       f.write("  \\autoBeamOff\n")
       f.write("  s1\n")
-      f.write("  {}{}1{} {}{}3{} | {}{}5{} {}{}6{} | {}{}8{} {}{}10{} | {}{}12{} {}{}13{}\n".format(note, octave, dur, note, octave+1, dur, note, octave+2, dur, note, octave+3, dur, note, octave+4, dur, note, octave+5, dur))
+      f.write("  {}{}1{} {}{}3{} | {}{}5{} {}{}6{} | {}{}8{} {}{}10{} | {}{}12{} {}{}13{}\n".format(note, octave, dur, note, octave, dur, note, octave, dur, note, octave, dur, note, octave, dur, note, octave, dur))
       f.write("}\n")
       
-print("Fichiers créés avec succès!")
+print("Fichiers crÃ©Ã©s avec succÃ¨s!")
