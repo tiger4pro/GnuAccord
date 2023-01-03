896947969bb453916f3545e7b38709f2b2331b68
tiger4pro <sprudhom@gmail.com>
2023-01-03 15:42:13 -0500

    00.00.05 Retour à la base sprudhom@gmail.com Écrire un programme en python crée un accord de do majeur ayant une durée d'une quadruple croche (64) dans fichier llilypond ayant pour syntaxe de nom de fichier : numéro d'octave, nom de la note en français, nom de l'accord et la durée. Ex.: 1_do_majeur_64. Ce programme crée un fichier nommé 1_do_majeur_64.ly qui contient le code Lilypond nécessaire pour générer un accord de do majeur ayant une durée de 64. Si vous souhaitez utiliser un autre nom de fichier ou une autre durée, il vous suffit de modifier les variables filename et content en conséquence.
    Pour utiliser ce programme, vous devez avoir installé Lilypond sur votre ordinateur. Vous pouvez alors utiliser la commande lilypond suivie du nom du fichier pour générer une partition en format PDF ou PostScript :
    ans le code Lilypond que j'ai fourni, l'accord a une durée de 64, soit une quadruple croche. Cela est indiqué par la notation <c e g>4 à la fin de la ligne \\key c \\major. La notation 4 indique que l'accord a une durée de 64, c'est-à-dire une quadruple croche.
    
    La notation <c e g> indique que l'accord est un accord de do majeur, qui est formé des notes de do, mi et sol.
    
    Si vous souhaitez utiliser une durée différente pour l'accord, il vous suffit de modifier la notation de durée. Par exemple, si vous souhaitez utiliser une durée de 32, soit une double croche, il vous suffit de remplacer 4 par 2.

--- GNUAccord/GNUAccord.py
+++ GNUAccord/GNUAccord.py
@@ -1,46 +1,34 @@
-﻿# Noms des notes de la musique en français
-notes = ['do', 'do#', 'réb', 'ré', 'ré#', 'mib', 'mi', 'fa', 'fa#', 'solb', 'sol', 'sol#', 'labb', 'la', 'la#', 'sib', 'si']
+﻿# Création du nom du fichier
+filename = "1_do_majeur_64_double_portee.ly"
 
-# Durée de la quadruple croche
-dur = "quadruplecroche"
+# Création du contenu du fichier
+content = """\
+\\version "2.20.0"
 
-def generate_lilypond_content(note, octave, dur):
-  """
-  Génère le contenu du fichier Lilypond pour un accord majeur.
-  
-  Args:
-    note (str): le nom de la note
-    octave (int): le numéro de l'octave
-    dur (str): la durée de l'accord
-    
-  Returns:
-    str: le contenu du fichier Lilypond
-  """
-  return f"""\\version "2.20.0"
+\\header {
+  title = "Double portée de piano"
+  composer = "Inconnu"
+}
 
-% {note} majeur
-{{
-  \\time 4/4
-  \\tempo 4=100
-  \\key {note} \\major
-  \\autoBeamOff
-  s1
-  {note}{octave}1\\{dur} {note}{octave+1}\\{dur} | {note}{octave+2}\\{dur} {note}{octave+3}\\{dur} | {note}{octave+4}\\{dur} {note}{octave+5}\\{dur}
-}}"""
+\\score {
+  \\new PianoStaff <<
+    \\new Staff {
+      \\clef "treble"
+      \\time 4/4
+      \\set Score.tempoHideNote = ##t
+      \\tempo 4=60
+      \\key c \\major
+      <c e g>4
+    }
+    \\new Staff {
+      \\clef "bass"
+      \\key c \\major
+      <c e g>4
+    }
+  >>
+}
+"""
 
-# Boucle sur tous les octaves (de 1 à 10)
-for octave in range(1, 11):
-  # Boucle sur toutes les notes de la gamme
-  for i, note in enumerate(notes):
-    # Remplace les dièses par le caractère "d"
-    note_for_filename = note.replace("#", "d")
-    
-    # Crée le nom du fichier
-    filename = f"{octave}_{note_for_filename}_majeur_{dur}.ly"
-    
-    # Ouvre le fichier en mode écriture
-    with open(filename, 'w') as f:
-      # Écrit le contenu du fichier
-      f.write(generate_lilypond_content(note, octave, dur))
-      
-print("Fichiers créés avec succès!")
+# Écriture du fichier
+with open(filename, "w") as f:
+    f.write(content)
